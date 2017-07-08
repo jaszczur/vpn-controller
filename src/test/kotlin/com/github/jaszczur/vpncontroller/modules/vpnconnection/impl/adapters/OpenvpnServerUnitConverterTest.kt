87e3071d7db756e44691c18e66636b222e227e2f
Jaszczur <jaszczur20@interia.pl>
2017-07-08 12:20:31 +0200

    fix tests

--- src/test/kotlin/com/github/jaszczur/vpncontroller/modules/vpnconnection/impl/adapters/OpenvpnServerUnitConverterTest.kt
+++ src/test/kotlin/com/github/jaszczur/vpncontroller/modules/vpnconnection/impl/adapters/OpenvpnServerUnitConverterTest.kt
@@ -1,6 +1,9 @@
-package com.github.jaszczur.vpncontroller.modules.vpnconnection.impl
+package com.github.jaszczur.vpncontroller.modules.vpnconnection.impl.adapters
 
+
+import com.github.jaszczur.vpncontroller.domain.ConnectableServer
 import com.github.jaszczur.vpncontroller.domain.Country
+import com.github.jaszczur.vpncontroller.domain.Protocol
 import com.github.jaszczur.vpncontroller.domain.ServerId
 import com.github.jaszczur.vpncontroller.modules.countries.Countries
 import org.assertj.core.api.Assertions.*
@@ -8,26 +11,25 @@ import org.junit.Test
 import org.mockito.BDDMockito.*
 import java.util.*
 
-class SystemdOpenvpnConversionsTest {
+class OpenvpnServerUnitConverterTest {
+
     val countries = mock(Countries::class.java)
-    val cut = SystemdOpenvpnConversions(countries)
+    val cut = OpenvpnServerUnitConverter(countries)
 
     @Test
     fun extractUnitInstance_shouldWork() {
         val result = cut.extractUnitInstance(
                 "openvpn-client@nord-nl-21-tcp.service loaded active running OpenVPN tunnel for nord/nl/21/tcp")
 
-        assertThat(result.block())
-                .isEqualTo("nord-nl-21-tcp")
+        assertThat(result)
+                .contains("nord-nl-21-tcp")
     }
 
     @Test
     fun extractUnitInstance_shouldReturnError_whenParsingFailed() {
         val unitListLine = "something completly different"
 
-        val result = cut.extractUnitInstance(unitListLine)
-
-        assertThat(catchThrowable { result.block() })
+        assertThat(catchThrowable { cut.extractUnitInstance(unitListLine) })
                 .hasMessageContaining(unitListLine)
     }
 
@@ -36,10 +38,10 @@ class SystemdOpenvpnConversionsTest {
         val country = Country("NL", "Netherlands")
         given(countries.byCode("NL")).willReturn(Optional.of(country))
 
-        val result = cut.unitInstanceToServerId("nord-nl-21-tcp")
+        val result = cut.fromUnitInstance("nord-nl-21-tcp")
 
-        assertThat(result.block())
-                .isEqualTo(ServerId(country, 21))
+        assertThat(result)
+                .contains(ConnectableServer(ServerId(country, 21), Protocol.TCP))
     }
 
     @Test
@@ -48,9 +50,7 @@ class SystemdOpenvpnConversionsTest {
         given(countries.byCode("NL")).willReturn(Optional.of(country))
         val unitInstance = "siała baba mak"
 
-        val result = cut.unitInstanceToServerId(unitInstance)
-
-        assertThat(catchThrowable { result.block() })
+        assertThat(catchThrowable { cut.fromUnitInstance(unitInstance) })
                 .hasMessageContaining(unitInstance)
     }
 }
