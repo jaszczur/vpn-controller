1947b31781d7e7201c166a949d26b293e82c9c56
Jaszczur <jaszczur20@interia.pl>
2017-07-08 10:10:49 +0200

    refactor

--- src/main/kotlin/com/github/jaszczur/vpncontroller/usecases/monitoring/SwitchConnectionUseCase.kt
+++ src/main/kotlin/com/github/jaszczur/vpncontroller/usecases/monitoring/SwitchConnectionUseCase.kt
@@ -1,10 +1,13 @@
 package com.github.jaszczur.vpncontroller.usecases.monitoring
 
+import com.github.jaszczur.vpncontroller.domain.Protocol
 import com.github.jaszczur.vpncontroller.domain.ServerId
 import com.github.jaszczur.vpncontroller.modules.stats.VpnStatsAdapter
 import com.github.jaszczur.vpncontroller.modules.vpnconnection.Monitoring
 import com.github.jaszczur.vpncontroller.modules.vpnconnection.VpnConnection
+import org.reactivestreams.Publisher
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.util.Loggers
 
 @Service
@@ -15,21 +18,40 @@ class SwitchConnectionUseCase(private val monitoring: Monitoring,
         private val logger = Loggers.getLogger(SwitchConnectionUseCase::class.java)
     }
 
-    fun beginMonitoring(config: MonitoringConfig): Unit {
-        println("Starting to monitor the connection")
+    fun beginMonitoring(config: MonitoringConfig, manualTrigger: Flux<Any>): Unit {
+        logger.info("Starting to monitor the connection")
+
+        streamOfSwitchAdvices(config, manualTrigger)
+                .transform(this::findBetterServer)
+                .transform(this.switchVpnServer(config.protocol))
+                .subscribe()
+    }
+
+    private fun streamOfSwitchAdvices(config: MonitoringConfig, manualTrigger: Flux<Any>): Flux<Advice> {
         val advisor = ConnectionAdvisor(config.windowSize, config.threshold)
-        monitoring.monitor()
+
+        val advicesFromTimer = monitoring.monitor()
                 .doOnNext { logger.debug("Measurement: $it") }
                 .map(advisor::giveAnAdvice)
                 .doOnNext { logger.debug("Advice: $it") }
                 .filter { it == Advice.SWITCH }
-                .flatMap { conn.active() }
-                .flatMap(this::findSimilarButBetter)
-                .doOnNext { logger.debug("Switching to: $it") }
-                .flatMap { conn.enable(it, config.protocol) }
-                .doOnNext { logger.info("Switched to: $it") }
-                .subscribe()
+
+        val advicesFromTrigger = manualTrigger.map { Advice.SWITCH }
+
+        return Flux.merge(advicesFromTimer, advicesFromTrigger)
     }
+
+    private fun findBetterServer(advices: Flux<Advice>): Publisher<ServerId> {
+        return advices.flatMap { conn.active() }
+                .flatMap(this::findSimilarButBetter)
+                .doOnNext { logger.debug("Found better server: $it") }
+    }
+
+    private fun switchVpnServer(protocol: Protocol): (Flux<ServerId>) -> Flux<ServerId> =
+            { serverIds: Flux<ServerId> ->
+                serverIds.flatMap { conn.enable(it, protocol) }
+                        .doOnNext { logger.info("Switched to: $it") }
+            }
 
     private fun findSimilarButBetter(serverId: ServerId) =
             stats.serverStats(serverId.country)
